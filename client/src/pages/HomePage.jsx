fa4c88e66ec4d0b0dfb5ca5a24a3b05c64490cf4
Sunmi Oye <114129193+summmyy@users.noreply.github.com>
2024-12-04 12:44:17 -0500

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
         const fetchEvents = async () => {
             try {
                 const response = await axios.get(
-                    'http://localhost:3000/api/events',
+                    'https://web-wizards-ui0t.onrender.com/api/events',
                     {
                         headers: {
                             Authorization: `Bearer ${localStorage.getItem(
@@ -41,7 +41,7 @@ const HomePage = () => {
         const checkUserAuth = async () => {
             try {
                 await axios.get(
-                    'http://localhost:3000/api/users/me',
+                    'https://web-wizards-ui0t.onrender.com/api/users/me',
                     {
                         headers: {
                             Authorization: `Bearer ${localStorage.getItem('token')}`,
